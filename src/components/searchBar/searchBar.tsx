6ea4be12042514c77f4c0952409ddc92420ef139
luisa garrozi <luisagarrozi@gmail.com>
2022-08-26 21:59:30 -0300

    added new message and get messages functions

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,12 +1,57 @@
-import { useState } from "react";
+import React from "react";
+import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
+import {
+  getSearchHistory,
+  SearchTerm
+} from "../../utils/editHistory";
+
 
 function SearchBar() {
-
 const [searchedTerm, setSearchedTerm] = useState('')
+const [errorMessage, setErrorMessage] = useState('')
+const [searches, setSearches] = React.useState<SearchTerm[]>(getSearchHistory);
+
+const handleNewTerm = (e: FormEvent) => {
+  e.preventDefault();
+
+  if (!searchedTerm || searchedTerm.trim() === '') {
+    setErrorMessage('Your request is empty.')
+    return
+  }
+
+  if (searchedTerm.trim().length < 3) {
+    setErrorMessage('Your request must be at least 3 characters long.')
+    return
+  }
+
+  if (searches.find(search => search.term === searchedTerm)) {
+    setErrorMessage('Your request already exists, check search history.')
+    return
+  }
+
+  createNewRequest(searchedTerm)
+}
+
+const createNewRequest = async (term: string) => {
+
+  const response = await api.post('/', { term });
+  if (response.data.status === 400) {
+  setErrorMessage('Your request failed, please try again.')
+  }
+  const newRequest = {
+    id: response.data.id,
+    term: term,
+  };
+
+  setSearches([...searches, newRequest]);
+  setSearchedTerm('');
+}
+
 
 return (
   <>
-  <form>
+  <form onSubmit={handleNewTerm}>
   <input
     value={searchedTerm}
     onChange={e => setSearchedTerm(e.target.value)}
@@ -14,6 +59,9 @@ return (
   />
   <button type="submit"></button>
   </form>
+  <div>
+    <span>{errorMessage}</span>
+  </div>
   </>
 )
 }
---
+++ src/utils/editHistory.ts
@@ -0,0 +1,12 @@
+export type SearchTerm = {
+  id: string;
+  term: string;
+}
+
+export function getSearchHistory() {
+  const oldItems = localStorage.getItem("axursearch");
+  const searchItems = (oldItems ? JSON.parse(oldItems) : []) as SearchTerm[];
+  return searchItems;
+}
+ 
+
